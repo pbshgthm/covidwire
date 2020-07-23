af2be66a543db81e2cfa6cb880dfadb7b0151893
pbshgthm <poobeshgowtham@live.com>
2020-07-23 16:40:54 +0530

    v4-init

--- client-ganga-v3/src/pages/Home.js
+++ client-ganga-v3/src/pages/Home.js
@@ -1,7 +1,45 @@
-import React, { useState, useEffect} from 'react';
-import TitleBar from '../components/TitleBar.js'
-import Setting from '../components/Setting.js'
-import Feed from '../components/Feed.js'
+import React, { useState, useEffect,useRef} from 'react';
+import TitleBar from '../components/TitleBar.js';
+import Setting from '../components/Setting.js';
+import Feed from '../components/Feed.js';
+import classNames from 'classnames';
+
+
+function FundUsBanner(props){
+	const _isMounted = useRef(true);
+	const [bannerSel,setBannerSel]=useState(false);
+	const [bannerClose,setBannerClose]=useState(false);
+
+	useEffect(() => {
+    	return () => {
+        	_isMounted.current = false;
+    	}
+  	}, []);
+
+	window.addEventListener('scroll', ()=>{
+		if(!_isMounted.current)return;
+		if (document.documentElement.scrollTop>300 ){
+		   setBannerSel(true)
+	   }
+	   else{
+		   setBannerSel(false)
+	   }
+	});
+
+	return(
+		<a href="https://milaap.org/fundraisers/support-sarigama-yerra-1" target="_blank" rel="noopener noreferrer">
+			<div className={classNames("FundUsBanner",{
+				"FundUsBannerShow":(bannerSel&&(!bannerClose))
+				})}>Click here to support our fundraising
+				<img className="BannerClose" src={require('../assets/close.png')} alt="back" onClick={(event)=>{
+					event.preventDefault();
+					setBannerClose(true)
+					return false;
+				}}/>
+			</div>
+		</a>
+	)
+}
 
 
 function Home(props){
@@ -21,6 +59,7 @@ function Home(props){
 	return (
 		<div className="FeedPage">
 			<TitleBar title="CovidWire"/>
+			<FundUsBanner/>
 			<img className="MainLogo" alt="CW Logo" src={require('../assets/logo-main.png')}/>
 			<Setting defaultState={stateSel} changeState={setStateSel} changeLang={setLangSel} defaultLang={langSel}/>
 			<Feed baseUrl={"feed/"+stateSel} langSel={langSel} pageSize={1}/>
