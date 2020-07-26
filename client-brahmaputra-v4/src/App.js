53c043e6d35a4ed51277402f038ebfc435651dce
pbshgthm <poobeshgowtham@live.com>
2020-07-26 18:12:52 +0530

    react-snap-test

--- client-brahmaputra-v4/src/App.js
+++ client-brahmaputra-v4/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import Home from './pages/Home.js'
 import SectionsList from './pages/SectionsList.js'
 import Hope from './pages/Hope.js'
-import Support from './pages/Support.js'
+import Blog from './pages/Blog.js'
 import About from './pages/About.js'
 
 
@@ -23,7 +23,7 @@ function App() {
 				<Route path="/home" component={Home}/>
 				<Route path="/hope" component={Hope}/>
 				<Route path="/sections" component={SectionsList}/>
-				<Route path="/support" component={Support}/>
+				<Route path="/blog" component={Blog}/>
 				<Route path="/about" component={About}/>
 			</Switch>
 			<div className="DesktopBlock">
---
+++ client-brahmaputra-v4/src/pages/Blog.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect} from 'react';
+import TitleBar from '../components/TitleBar.js'
+
+
+
+
+function Blog(props){
+
+	return (
+		<div className="FeedPage">
+			<TitleBar title="CW Speaks"/>
+		</div>
+	);
+}
+
+export default Blog;
